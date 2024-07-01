d6ad484417fdadcef27375f8f617143eb7e35979
debcamila <camiladeborah72@gmail.com>
2024-07-01 18:26:48 -0300

    :sparkles: feat: add routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
+import { RoutesApp } from "./routes";
+
 function App() {
   return (
     <div className="App">
-      <h1>Projeto do curso: </h1>
-      <p>React Js do zero ao avançado na prática</p>
+      <RoutesApp />
     </div>
   );
 }
