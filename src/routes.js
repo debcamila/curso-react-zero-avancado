477ad055181457058c17c1c3f532ae9a87fb91f0
debcamila <camiladeborah72@gmail.com>
2024-07-01 18:35:01 -0300

    :sparkles: feat: add header

--- src/routes.js
+++ src/routes.js
@@ -4,10 +4,12 @@ import Home from "./pages/Home";
 import Movie from "./pages/Movie";
 
 import React from "react";
+import Header from "./components/Header";
 
 export const RoutesApp = () => {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/movie/:id" element={<Movie />} />
